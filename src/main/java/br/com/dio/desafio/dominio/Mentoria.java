910f122124fcd5832a6e908da1fe9c8f86e5a65b
WesRush <gualbertoweslley@gmail.com>
2025-08-09 14:43:30 -0300

    final

--- src/main/java/br/com/dio/desafio/dominio/Mentoria.java
+++ src/main/java/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,20 +2,16 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
+
    private LocalDate data;
 
     public Mentoria() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    @Override
+    public double calcularXp() {
+        return xpPadrao + 20d;
     }
 
     public LocalDate getData() {
@@ -26,20 +22,16 @@ public class Mentoria {
         this.data = data;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
+
+
 }
